f54de79528ccd53eab1b763898fd7c72fbf372a1
Samsung T5 - Program <Samsung T5 - Program>
2020-07-21 13:54:38 +0800

    add remark

--- init_server_shell/init_server.sh
+++ init_server_shell/init_server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SUDO_USER="username"
 SUDO_PASSWORD="password"
+SSH_FILE=/etc/ssh/sshd_config
 SSH_PORT=22
 MARIADB_PORT=3306
 REDIS_PORT=6379
@@ -36,19 +37,27 @@ EOF
 fi
   apt update \
 	&& apt install -y unzip \
+  && echo "Start creating user..." \
 	&& useradd -m $SUDO_USER -g sudo -s /bin/bash -d /home/$SUDO_USER \
 	&& echo $SUDO_USER:$SUDO_PASSWORD |chpasswd \
-	&& sed -i "s/#Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config \
-	&& sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config \
+  && echo "End of creating user..." \
+  && echo "Start configuring the SSH file..." \
+	&& sed -i "s/#Port 22/Port $SSH_PORT/" $SSH_FILE \
+	&& sed -i 's/PermitRootLogin yes/PermitRootLogin no/' $SSH_FILE \
+  && echo "End of configuration SSH file..." \
 	&& su - $SUDO_USER  <<!
 cd /home/$SUDO_USER \
 	&& echo $SUDO_PASSWORD | sudo -S apt install -y git \
 	&& sudo apt-get -y update \
+  && echo "Prepare to install docker..." \
 	&& sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common \
+	&& apt-get install -y gnupg2 \
 	&& curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | sudo apt-key add - \
 	&& sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/debian $(lsb_release -cs) stable" \
 	&& sudo apt-get -y update \
 	&& sudo apt-get -y install docker-ce docker-ce-cli containerd.io \
+  && echo "Docker installed successfully..." \
+  && echo "Prepare to install docker-compose..." \
 	&& if [ "$INCHINA" == "true" ]; then sudo curl -L "https://get.daocloud.io/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose; else sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose; fi \
   && sudo chmod +x /usr/local/bin/docker-compose \
   && sudo mv ../docker_compose /www \
@@ -58,6 +67,8 @@ cd /home/$SUDO_USER \
   && sed -i "s/Postgres Port/$POSTGRES_PORT/" /www/docker-compose.yml \
   && sed -i "s/postgres_user/$POSTGRES_USER/" /www/docker-compose.yml \
   && sed -i "s/postgres_password/$POSTGRES_PASSWORD/" /www/docker-compose.yml \
+  && echo "Docker-compose installed successfully..." \
+  && echo "Configuring iptables..." \
   && (sudo bash -c "cat <<EOF >/etc/iptables.rules
 *filter
 -P INPUT DROP
@@ -78,6 +89,8 @@ EOF") \
 /sbin/iptables-restore < /etc/iptables.rules
 EOF") \
   && sudo chmod +x /etc/network/if-pre-up.d/iptables \
+  && echo "Iptables configuration complete..." \
+  && echo "Restart service..." \
   && sudo systemctl restart docker \
 	&& sudo systemctl restart ssh
 !
