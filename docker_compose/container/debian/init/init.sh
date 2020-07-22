73e1910fd2424aa9247b94eaea4cc33f0d9baf54
Samsung T5 - Program <Samsung T5 - Program>
2020-07-22 08:27:59 +0800

    \r\n

--- docker_compose/container/debian/init/init.sh
+++ docker_compose/container/debian/init/init.sh
@@ -2,4 +2,4 @@
 sleep 5
 echo "Running Debian Successful."
 #cd /www/wwwroot/example1.com/ && chmod +x main && nohup ./main &
-#cd /www/wwwroot/example2.com/ && chmod +x main && nohup ./main &
+# cd /www/wwwroot/example2.com/ && chmod +x main && nohup ./main &
